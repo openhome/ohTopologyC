a3f5c59bb05c7ac67422e7e544cee87aaa153b4d
Eamonn Brady <eamonn.brady@linn.co.uk>
2014-03-20 11:25:01 +0000

    Added missing files

---
+++ OpenHome/MetaData.cpp
@@ -0,0 +1,62 @@
+#include <OpenHome/MetaData.h>
+
+
+using namespace OpenHome;
+using namespace OpenHome::Av;
+
+
+
+///////////////////////////////////////////////////////////////
+
+
+IInfoMetadata* InfoMetadata::Empty()
+{
+    return(new InfoMetadata());
+}
+
+
+InfoMetadata::InfoMetadata()
+{
+//    iMetadata = null;
+//    iUri = null;
+}
+
+
+InfoMetadata::InfoMetadata(IMediaMetadata& aMetadata, const Brx& aUri)
+    :iMetadata(&aMetadata)
+    ,iUri(aUri)
+{
+}
+
+
+IMediaMetadata& InfoMetadata::Metadata()
+{
+    return (*iMetadata);
+}
+
+
+Brn InfoMetadata::Uri()
+{
+    return iUri;
+}
+
+
+///////////////////////////////////////////////////////////////
+
+InfoMetatext::InfoMetatext()
+{
+//    iMetatext = null;
+}
+
+
+InfoMetatext::InfoMetatext(IMediaMetadata& aMetatext)
+    :iMetatext(&aMetatext)
+{
+}
+
+
+IMediaMetadata& InfoMetatext::Metatext()
+{
+    return(*iMetatext);
+}
+
---
+++ OpenHome/MetaData.h
@@ -0,0 +1,89 @@
+#ifndef HEADER_OHTOPOLOGYC_META_DATA
+#define HEADER_OHTOPOLOGYC_META_DATA
+
+#include <OpenHome/OhNetTypes.h>
+#include <OpenHome/Buffer.h>
+
+
+namespace OpenHome
+{
+namespace Av
+{
+
+
+//////////////////////////////////////////////////////////////////////
+
+class IInfoDetails
+{
+    virtual TUint BitDepth() = 0;
+    virtual TUint BitRate() = 0;
+    virtual Brn CodecName() = 0;
+    virtual TUint Duration() = 0;
+    virtual TBool Lossless() = 0;
+    virtual TUint SampleRate() = 0;
+};
+
+//////////////////////////////////////////////////////////////////////
+
+class IMediaMetadata
+{
+
+};
+
+//////////////////////////////////////////////////////////////////////
+
+class IInfoMetadata
+{
+    virtual IMediaMetadata& Metadata() = 0;
+    virtual Brn Uri() = 0;
+};
+
+//////////////////////////////////////////////////////////////////////
+
+class IInfoMetatext
+{
+    virtual IMediaMetadata& Metatext() = 0;
+};
+
+//////////////////////////////////////////////////////////////////////
+
+class InfoMetadata : public IInfoMetadata
+{
+public:
+    static IInfoMetadata* Empty();
+    InfoMetadata(IMediaMetadata& aMetadata, const Brx& aUri);
+    virtual IMediaMetadata& Metadata();
+    virtual Brn Uri();
+
+private:
+    InfoMetadata();
+
+private:
+    IMediaMetadata* iMetadata;
+    Brn iUri;
+};
+
+//////////////////////////////////////////////////////////////////////
+
+class InfoMetatext : public IInfoMetatext
+{
+
+public:
+    InfoMetatext(IMediaMetadata& aMetatext);
+    IMediaMetadata& Metatext();
+
+private:
+    /*internal */InfoMetatext();
+
+private:
+    IMediaMetadata* iMetatext;
+};
+
+
+
+} // namespace OpenHome
+
+} // namespace Av
+
+
+#endif // HEADER_OHTOPOLOGYC_META_DATA
