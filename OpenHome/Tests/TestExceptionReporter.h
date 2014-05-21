31b1e8c729072a81b46eedf85e1ee909dc06986d
unknown <eamonnb@PC815.linn.co.uk>
2014-05-21 11:53:26 +0100

    Test class separated into own file

---
+++ OpenHome/Tests/TestExceptionReporter.h
@@ -0,0 +1,35 @@
+#ifndef HEADER_TEST_EXCEPTION_REPORTER
+#define HEADER_TEST_EXCEPTION_REPORTER
+
+#include <OpenHome/Private/OptionParser.h>
+#include <OpenHome/OsWrapper.h>
+
+
+using namespace OpenHome;
+using namespace OpenHome::TestFramework;
+
+
+namespace OpenHome {
+
+class TestExceptionReporter : public IExceptionReporter
+{
+public:
+    TestExceptionReporter() :iCount(0) ,iCountStd(0) {}
+    virtual void Report(Exception& /*aException*/) { iCount++;}
+    virtual void Report(std::exception& /*aException*/) { iCountStd++;}
+
+    TUint Count() const { return(iCount); }
+    TUint CountStd() const { return(iCountStd); }
+
+private:
+    TUint iCount;
+    TUint iCountStd;
+};
+
+
+} // namespace OpenHome
+
+
+/////////////////////////////////////////////////////////////////////
+
+#endif // HEADER_TEST_EXCEPTION_REPORTER
