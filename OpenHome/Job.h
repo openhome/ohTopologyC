850f9af1c859eb77bbec5c759245e39a0437e4fe
Eamonn Brady <eamonn.brady@linn.co.uk>
2014-12-09 11:45:35 +0000

    Removed commented code

--- OpenHome/Job.h
+++ OpenHome/Job.h
@@ -12,12 +12,8 @@
 
 namespace OpenHome
 {
-
 namespace Av
 {
-
-//class CallbackHandler;
-
 
 class Job
 {
@@ -50,80 +46,12 @@ private:
     TBool iCancelled;
 };
 
-
-////////////////////////////////////////////////////////////////////////////
-/*
-class JobDone
-{
-public:
-    JobDone();
-
-    void SetResult(TBool aResult);
-    void Cancel();
-    Job* GetJob();  //  shouldn't this return a ref?
-
-private:
-    Job* iJob;
-};
-*/
 ///////////////////////////////////////////////////////////////////////////
 
 struct AsyncCbArg
 {
     Net::IAsync* iAsync;
     void* iArg;
-};
-
-///////////////////////////////////////////////////////////////////////////
-/*
-class Job2 : private Thread
-{
-public:
-    Job2();
-    void SetCallback(FunctorGeneric<AsyncCbArg*> aAction, void* aArg);
-    void CallbackComplete();
-    void Run();
-    Net::FunctorAsync AsyncCb();
-    void AsyncComplete(Net::IAsync& aAsync);
-    void Cancel();
-
-private:
-    FunctorGeneric<AsyncCbArg*> iCallback;
-    void* iArg;
-    AsyncCbArg* iCbArg;
-    TBool iCancelled;
-    mutable Mutex iMutex;
-};
-*/
-///////////////////////////////////////////////////////////////////////////
-
-class Job3;
-
-///////////////////////////////////////////////////////////////////////////
-
-/*
-class IJobManager
-{
-public:
-    virtual Job3& GetJob() = 0;
-    virtual void ReleaseJob(Job3& aJob) = 0;
-};
-*/
-///////////////////////////////////////////////////////////////////////////
-
-class JobManager //: public IJobManager
-{
-private:
-    static const TUint kJobCount = 10;
-
-public:
-    JobManager();
-    virtual ~JobManager();
-    virtual Job3& GetJob();
-    virtual void ReleaseJob(Job3& aJob);
-
-private:
-    Fifo<Job3*> iJobs;
 };
 
 ///////////////////////////////////////////////////////////////////////////
