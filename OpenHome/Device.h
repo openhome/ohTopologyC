356ebf529ba0fcd21fce3dfd8c03dbe55ce74f24
JJ Hassan <John.Hassan@linn.co.uk>
2015-12-08 16:08:54 +0000

    InjectorDeviceTest

--- OpenHome/Device.h
+++ OpenHome/Device.h
@@ -136,5 +136,30 @@ private:
     IInjectorDevice* iOn;
 };
 
+///////////////////////////////////////////////////////////////
+
+class InjectorDeviceTest : public IInjectorDevice
+{
+public:
+  InjectorDeviceTest(INetwork& aNetwork);
+  ~InjectorDeviceTest();
+public: //IInjectorDevice
+  Brn Udn() override;
+  INetwork& Network() const override;
+  void Create(FunctorGeneric<IProxy*>, EServiceType aServiceType, IDevice& aDevice) override;
+  TBool HasService(EServiceType aServiceType) override;
+  TBool Wait() override;
+public: //IJoinable
+  void Join(Functor aAction) override;
+  void Unjoin(Functor aAction) override;
+public:  // IMockable
+  void Execute(ICommandTokens& aTokens) override;
+public: // IDisposable
+  void Dispose() override;
+private:
+  Bws<37> iUdn;
+  INetwork& iNetwork;
+};
+
 } // Topology
 } // OpenHome
